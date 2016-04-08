abd35ba879370b23fb90791911b3323354d4b58b
Ahmed <Ahmed@DESKTOP-E1NT9D7>
2016-04-08 17:57:38 -0400

    first

---
+++ src/com/pmgcrise/pmgcmusicplayer/MusicController.java
@@ -0,0 +1,19 @@
+package com.pmgcrise.pmgcmusicplayer;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+public class MusicController extends MediaController {
+
+	public MusicController(Context c)
+	{
+		super(c);
+	}
+	
+	public void hide()
+	{
+		
+	}
+	
+	                                        
+}
---
+++ src/com/pmgcrise/pmgcmusicplayer/SongAdapter.java
@@ -0,0 +1,59 @@
+package com.pmgcrise.pmgcmusicplayer;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import java.util.ArrayList;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class SongAdapter extends BaseAdapter{
+	private ArrayList<Song> songs;
+	//private LayoutInflater songInf;
+	private Context context;
+	
+	public SongAdapter(Context c, ArrayList<Song> theSongs){
+		  songs=theSongs;
+		  //songInf=LayoutInflater.from(c);
+		  context = c;
+		}
+
+	@Override
+	public int getCount() {
+		return songs.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+	  //map to song layout
+	  LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		
+	  LinearLayout songLay = (LinearLayout)inflater.inflate(R.layout.song, parent, false);
+	  //get title and artist views
+	  TextView songView = (TextView)songLay.findViewById(R.id.song_title);
+	  TextView artistView = (TextView)songLay.findViewById(R.id.song_artist);
+	  //get song using position
+	  Song currSong = songs.get(position);
+	  //get title and artist strings
+	  songView.setText(currSong.getTitle());
+	  artistView.setText(currSong.getArtist());
+	  //set position as tag
+	  songLay.setTag(position);
+	  return songLay;
+	}
+
+}
